5c8153dd1374e317d06e4fc823e8869018d42544
joostlek <joostlek@outlook.com>
2019-08-16 15:19:48 +0200

    Create TextLink

--- components/interactions/TextLink.vue
+++ components/interactions/TextLink.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
+  <div @click="callback">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'TextLink',
+  props: {
+    callback: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    font-weight: 500;
+    font-family: Roboto, serif;
+    font-size: 24px;
+    line-height: 140%;
+    letter-spacing: 1.5px ;
+    color: #72C9E0;
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+  }
+  div:after {
+    position: absolute;
+    display: block;
+    height: 2px;
+    width: 105%;
+    border-radius: 1px;
+    background-color: #DBEFF4;
+    content: '';
+    left: -5px;
+    bottom: 0;
+  }
+</style>
