ddc5f44e4b1c2bea5aaf36a59147735e88dee148
info@noudadrichem.com <info@noudadrichem.com>
2019-08-16 13:19:05 +0200

    layout changes in css for heading

--- components/Heading.vue
+++ components/Heading.vue
@@ -3,7 +3,7 @@
     <div class="heading-content">
       <img src="/logo/indicium-logo-top.svg" alt="Indicium Logo" />
 
-      <h1>Wij zijn de <span class="medium">studie</span>verenigign voor HBO-ICT van Hogeschool Utrecht</h1>
+      <h1>Wij zijn de <span class="medium">studie</span>vereniging voor HBO-ICT van Hogeschool Utrecht</h1>
 
       <div class="buttons">
         <Button size="l">Wordt lid</Button>
@@ -28,19 +28,18 @@ export default {
 
 <style lang="scss" scoped>
 .heading {
-  height: 100vh;
-  max-height: 904px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 128px;
-  background-color: #f2f2f2;
+  justify-content: center;
+  padding: 128px 0 192px;
 
   &-content {
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     h1 {
-      max-width: 568px;
+      max-width: 512px;
     }
 
     .buttons {
