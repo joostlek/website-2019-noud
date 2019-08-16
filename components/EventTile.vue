732360e815690c7f51307a30cca5eaf279e58efd
info@noudadrichem.com <info@noudadrichem.com>
2019-08-16 15:51:11 +0200

    events with colors for categories

--- components/EventTile.vue
+++ components/EventTile.vue
@@ -1,5 +1,8 @@
 <template>
   <a class="event-tile container" :href="event.url">
+    <div class="color-container">
+      <div v-for="(colorObj, idx) in getCategoryColors" :key="idx" :style="{ backgroundColor: `#${colorObj.hex}`}"></div>
+    </div>
     <h5>{{ toDateString }}</h5>
     <h4>{{ event.title }}</h4>
     <p class="small">{{ event.description }}</p>
@@ -13,6 +16,10 @@ export default {
     event: {
       type: Object,
       required: true,
+    },
+    allCategories: {
+      type: Array,
+      required: true
     }
   },
   computed: {
@@ -20,8 +27,15 @@ export default {
       const date = new Date(this.event.date * 1000)
       return `${date.getDate()} ${this.getMonthAsString(date.getMonth())} ${date.getFullYear()}`
     },
-    getCourseColor() {
-      return '#ff00ff'
+    getCategoryColors() {
+      return this.allCategories.reduce((activeCategories, category) => {
+        const categories = this.event.categories.map(c => c)
+        if (categories.includes(category.courseTitle)) {
+          activeCategories.push(category)
+        }
+
+        return activeCategories
+      }, [])
     }
   },
   methods: {
@@ -51,19 +65,34 @@ export default {
 <style lang='scss' scoped>
 .event-tile {
   display: inline-flex;
+  position: relative;
   min-width: 300px;
   flex: 1;
   flex-direction: column;
-  border-top: 4px solid #72c9e1;
   flex-direction: wrap;
   margin: 16px;
   background: #f3f8fa;
-  padding: 10px;
+  padding: 16px;
   box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2);
   transition: box-shadow 300ms;
 
   &:hover {
     box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.5);
+  }
+
+  .color-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 4px;
+    width: 100%;
+    display: flex;
+
+    div {
+      display: inline-flex;
+      flex: 1;
+      height: 100%;
+    }
   }
 
   h5 {
--- components/Events.vue
+++ components/Events.vue
@@ -2,8 +2,16 @@
   <div class="events-container">
     <h2 class="center">Opkomende activiteiten</h2>
 
+    <div class="categories">
+      <ul>
+        <li v-for="(category, idx) in allCategories" :key="idx">
+          <span :style="{backgroundColor: `#${category.hex}`}"></span>{{ category.courseTitle }}
+        </li>
+      </ul>
+    </div>
+
     <div class="events">
-      <EventTile v-for="(event, idx) in events" :key="idx" :event="event" />
+      <EventTile v-for="(event, idx) in events" :key="idx" :event="event" :allCategories="allCategories" />
     </div>
   </div>
 </template>
@@ -23,43 +31,65 @@ export default {
         description: 'Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar.',
         date: 1257898,
         url: '#',
-        course: 'BIM'
+        categories: ['AI']
       },
       {
         title: 'Title',
         description: 'Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar.',
         date: 1257898,
         url: '#',
-        course: 'SD'
+        categories: ['SD', 'BIM']
       },
       {
         title: 'Title',
         description: 'Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar.',
-        date: 1257898,
+        date: 1565961457,
         url: '#',
-        course: 'TI'
+        categories: ['TI']
       },
       {
         title: 'Title',
         description: 'Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar.',
-        date: 1257898,
+        date: 12578989,
         url: '#',
-        course: 'BIM'
+        categories: ['SD', 'TI', 'SNE', 'BIM', 'AI']
       },
       {
         title: 'Title',
         description: 'Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar. Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar.',
         date: 1257898,
         url: '#',
-        course: 'SD'
+        categories: ['SD']
       },
       {
         title: 'Title',
         description: 'Integer dictum arcu vitae magna pulvinar interdum. Quisque semper arcu sed mauris efficitur pulvinar.',
         date: 1257898,
         url: '#',
-        course: 'TI'
+        categories: ['TI', 'SD', 'AI']
       }
+    ],
+    allCategories: [
+      {
+        courseTitle: 'SD',
+        hex: 'E17272'
+      },
+      {
+        courseTitle: 'TI',
+        hex: 'A2E172'
+      },
+      {
+        courseTitle: 'SNE',
+        hex: 'E1DB72'
+      },
+      {
+        courseTitle: 'BIM',
+        hex: '727AE1'
+      },
+      {
+        courseTitle: 'AI',
+        hex: '72E1AD'
+      },
     ]
   })
 }
@@ -70,13 +100,32 @@ export default {
   max-width: 1084px;
   margin: 0 auto;
 
+  .categories {
+    ul {
+      display: flex;
+      justify-content: center;
+
+      li {
+        display: inline-flex;
+        align-items: center;
+        padding: 16px;
+
+        span {
+          display: inline-flex;
+          width: 24px;
+          height: 24px;
+          background-color: #f2f2f2;
+          margin-right: 8px;
+        }
+      }
+    }
+  }
+
   .events {
     display: flex;
     justify-content: space-between;
     flex-direction: row;
     flex-wrap: wrap;
-
-    .event-tile {}
   }
 
 }
