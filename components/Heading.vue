4ab365cd2bd5111594500cbf8333180775511878
info@noudadrichem.com <info@noudadrichem.com>
2019-08-16 13:14:02 +0200

    first heading draft

--- components/Heading.vue
+++ components/Heading.vue
@@ -1,5 +1,58 @@
 <template>
-  <div>
-    Heading
+  <div class="heading">
+    <div class="heading-content">
+      <img src="/logo/indicium-logo-top.svg" alt="Indicium Logo" />
+
+      <h1>Wij zijn de <span class="medium">studie</span>verenigign voor HBO-ICT van Hogeschool Utrecht</h1>
+
+      <div class="buttons">
+        <Button size="l">Wordt lid</Button>
+        <TextLink>Bekijk agenda</TextLink>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Button from '../components/interactions/button'
+import TextLink from '../components/interactions/TextLink'
+
+export default {
+  name: 'Heading',
+  components: {
+    Button,
+    TextLink
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.heading {
+  height: 100vh;
+  max-height: 904px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 128px;
+  background-color: #f2f2f2;
+
+  &-content {
+    text-align: center;
+
+    h1 {
+      max-width: 568px;
+    }
+
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-self: center;
+
+      .btn {
+        margin-right: 32px;
+      }
+    }
+  }
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -33,10 +33,14 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
 body {
-  background-color: #72c9e0;
   font-family: 'Roboto', sans-serif;
 }
 </style>
